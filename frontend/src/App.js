d7d0c898e849e8416ad868087bdc3e79a52c396d
arcanrun <arcan.run@gmail.com>
2018-10-27 21:48:12 +0300

    front fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import "./App.css";
 import { Layout, Menu, Breadcrumb, Icon, Avatar, Card } from "antd";
 
 // costum
-import { ProjectCard } from "./ProjectCard";
+import ProjectCard from "./ProjectCard";
 
 const { Header, Content, Footer, Sider } = Layout;
-const { Meta } = Card;
+
 const SubMenu = Menu.SubMenu;
 class App extends Component {
   state = {
@@ -18,7 +18,23 @@ class App extends Component {
     this.setState({ collapsed });
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hits: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/project/list/").then(
+      response => this.setState({ hits: response.json() })
+      // console.log(response.json())
+    );
+  }
+
   render() {
+    const { hits } = this.state;
     return (
       <Layout style={{ minHeight: "100vh" }}>
         <Sider
@@ -58,54 +74,12 @@ class App extends Component {
         <Layout>
           <Content style={{ margin: "10px 30px" }}>
             <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
-              <ProjectCard
-                cover={
-                  <img
-                    alt="example"
-                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                  />
-                }
-                actions={[
-                  <Icon type="setting" />,
-                  <Icon type="edit" />,
-                  <Icon type="ellipsis" />
-                ]}
-              >
-                <Meta
-                  avatar={
-                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                  }
-                  title="Card title"
-                  description="This is the description"
-                />
-              </ProjectCard>
-
-              <ProjectCard
-                cover={
-                  <img
-                    alt="example"
-                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                  />
-                }
-                actions={[
-                  <Icon type="setting" />,
-                  <Icon type="edit" />,
-                  <Icon type="ellipsis" />
-                ]}
-              >
-                <Meta
-                  avatar={
-                    <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-                  }
-                  title="Card title"
-                  description="This is the description"
-                />
-              </ProjectCard>
+              {hits.map(hit => (
+                <ProjectCard title={hit} />
+              ))}
             </div>
           </Content>
-          <Footer style={{ textAlign: "center" }}>
-            Ant Design ©2018 Created by Ant UED
-          </Footer>
+          <Footer style={{ textAlign: "center" }}>Desing by Lazy Nerds</Footer>
         </Layout>
       </Layout>
     );
--- frontend/src/ProjectCard.js
+++ frontend/src/ProjectCard.js
@@ -1,3 +1,4 @@
+import React, { Component } from "react";
 import styled from "styled-components";
 import { Icon, Avatar, Card } from "antd";
 
@@ -7,4 +8,28 @@ const ProjectCard = styled(Card)`
   margin-bottom: 20px;
 `;
 
-export { ProjectCard };
+const { Meta } = Card;
+
+export default ({ title, sum, have_sum, author_username, deadline }) => (
+  <ProjectCard
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <Icon type="setting" />,
+      <Icon type="edit" />,
+      <Icon type="ellipsis" />
+    ]}
+  >
+    <Meta
+      avatar={
+        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+      }
+      title={title}
+      description="This is the description"
+    />
+  </ProjectCard>
+);
