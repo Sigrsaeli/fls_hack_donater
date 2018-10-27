4c01940a0f9b5dee3bc52d824d466f349632f8ea
Святослав <kovalev.svyatoslav42@gmail.com>
2018-10-27 15:08:30 +0300

    backend

---
+++ backend/donater/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Projects(models.Model):
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField(max_length=255)
+    description = models.TextField()
+    sum = models.IntegerField()
+    tags = models.TextField()
+    deadline = models.DateTimeField()
+
+class Transaction(models.Model):
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    target_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    sum = models.IntegerField()
